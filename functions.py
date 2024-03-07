ab4551ea3dac900b5d72014d63d407d5f7ecc8b7
IgorJovita <igor.jovita@hotmail.com>
2024-03-06 23:04:33 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -323,7 +323,7 @@ def cadastra_cliente(nome, data, telefone, cpf, estado):
         )
 
         cursor.execute("SELECT tipo from reserva where id_cliente = %s", (id_cliente, ))
-        tipo = cursor.fetchone()
+        tipo = cursor.fetchone()[0]
         st.write(tipo)
         cursor.execute(f"UPDATE reserva set nome_cliente = '{nome}' WHERE id_cliente = {id_cliente} and data = '{data}'")
 
