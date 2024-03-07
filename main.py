fbee2c8944adba840dd724c8e879360a417be133
IgorJovita <igor.jovita@hotmail.com>
2024-03-06 23:25:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -161,6 +161,7 @@ if st.session_state.count == 4:
         radio_dd = st.radio(label=dd, options=opcoes, horizontal=True, index=None)
         radio_diabetes = st.radio(label=diabetes, options=opcoes, horizontal=True, index=None)
         radio_hemorragia = st.radio(label=hemorragia, options=opcoes, horizontal=True, index=None)
+        col1, col2 = st.columns(2)
         with colun2:
             if st.form_submit_button(botao2):
                 st.session_state.termo_medico.append((radio_gravida, radio_remedio, radio_cardiaca, radio_asma, radio_pulmonar, radio_epilepsia, radio_enjoo, radio_dd, radio_coluna, radio_diabetes, radio_ouvido, radio_hemorragia))
