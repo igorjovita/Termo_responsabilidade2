65a87774146ca15113726fd17f3e26ea714ff900
IgorJovita <igor.jovita@hotmail.com>
2024-03-18 09:19:20 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -353,6 +353,6 @@ def insert_termo_medico(id_cliente, id_termo_cliente, gravida, remedio, cardiaca
     mydb.connect()
 
     cursor.execute(
-        "INSERT INTO termo_medico (id_cliente, id_termo_cliente, gravida, remedio, doenca_cardiaca, asma, doenca_pulmonar, epilepsia, enjoo, dd, coluna, diabetes, ouvido, hemorragia, sinusite, cirurgia, nome_cirurgia, tempo_cirurgia, viagem,  menor, bebida) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        "INSERT INTO termo_medico (id_cliente, id_termo_cliente, gravida, remedio, doenca_cardiaca, asma, doenca_pulmonar, epilepsia, enjoo, dd, coluna, diabetes, ouvido, hemorragia, sinusite, cirurgia, nome_cirurgia, tempo_cirurgia, viagem,  menor, bebida) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
         (id_cliente, id_termo_cliente, gravida, remedio, cardiaca, asma, pulmonar, epilepsia, enjoo, dd, coluna,
          diabetes, ouvido, hemorragia, sinusite, input_cirurgia, nome_cirurgia, input_tempo_cirurgia, viagem, menor, bebida))
